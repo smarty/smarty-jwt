4cdc27694a661018d5615daca7bdc9fbd8309419
Michael Whatcott <mdwhatcott@gmail.com>
2019-10-08 11:17:30 -0600

    Decoding expiration.

--- decoder.go
+++ decoder.go
@@ -3,6 +3,7 @@ package jwt
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"strings"
 )
 
@@ -20,12 +21,20 @@ func (this Decoder) Decode(token string, claims interface{}) error {
 
 	var m map[string]interface{}
 	_ = json.Unmarshal(payloadBytes, &m)
+	fmt.Println(m)
 
-	if issuer, ok := claims.(Issuer); ok {
-		issuer.SetIssuer(m["iss"].(string)) // TODO protect cast
-	}
+	issuer := claims.(Issuer)           // TODO protect cast
+	issuer.SetIssuer(m["iss"].(string)) // TODO protect cast
+
+	expiration := claims.(Expiration)  // TODO protect cost
+	value := int64(m["exp"].(float64)) // TODO protect cast
+	expiration.SetExpiration(value)
 
 	return nil
+}
+
+type Expiration interface {
+	SetExpiration(int64)
 }
 
 type Issuer interface {
--- decoder_test.go
+++ decoder_test.go
@@ -8,12 +8,12 @@ import (
 )
 
 func TestDecoderFixture(t *testing.T) {
-    gunit.Run(new(DecoderFixture), t)
+	gunit.Run(new(DecoderFixture), t)
 }
 
 type DecoderFixture struct {
-    *gunit.Fixture
-    decoder *Decoder
+	*gunit.Fixture
+	decoder *Decoder
 }
 
 func (this *DecoderFixture) Setup() {
@@ -21,20 +21,25 @@ func (this *DecoderFixture) Setup() {
 }
 
 func (this *DecoderFixture) TestDecode() {
-	token :="eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ."
+	token := "eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ."
 	var claims parsedPayload
 
 	err := this.decoder.Decode(token, &claims)
 
 	this.So(err, should.BeNil)
 	this.So(claims.Issuer, should.Equal, "joe")
+	this.So(claims.Expiration, should.Equal, 1300819380)
 }
 
-
 type parsedPayload struct {
-	Issuer string
+	Issuer     string
+	Expiration int64
 }
 
 func (this *parsedPayload) SetIssuer(value string) {
 	this.Issuer = value
+}
+
+func (this *parsedPayload) SetExpiration(value int64) {
+	this.Expiration = value
 }
