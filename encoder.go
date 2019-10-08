5128b5404a55e85c82ba0d9498c3e4068d53de5f
Duncan Beutler <duncan@smartystreets.com>
2019-10-08 10:14:27 -0600

    Cleaned up rouge print statement

--- encoder.go
+++ encoder.go
@@ -2,7 +2,6 @@ package jwt
 
 import (
 	"encoding/base64"
-	"fmt"
 )
 
 type Encoder struct {
@@ -16,7 +15,6 @@ func NewEncoder(serializer Serializer) *Encoder {
 func (this *Encoder) Encode(claims interface{}) string {
 	header := this.serializer.Serialize(map[string]string{"alg": "none"})
 	payload := this.serializer.Serialize(claims)
-	fmt.Println(string(payload))
 	encodedHeader := base64.RawStdEncoding.EncodeToString(header)
 	encodedBody := base64.RawStdEncoding.EncodeToString(payload)
 	return encodedHeader + "." + encodedBody + "."
