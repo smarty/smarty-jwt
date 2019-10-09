f6ce0aa473169f9139f4d56bd441a66d9958f7f2
Duncan Beutler <duncan@smartystreets.com>
2019-10-09 14:22:26 -0600

    Decoder validates signature

--- decoder.go
+++ decoder.go
@@ -1,13 +1,16 @@
 package jwt
 
 import (
+	"crypto/subtle"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"strings"
 )
 
 type Decoder struct {
 	claimCallbacks []ClaimCallback
+	secret         []byte
 }
 
 func NewDecoder(claimCallbacks ...ClaimCallback) *Decoder {
@@ -15,9 +18,26 @@ func NewDecoder(claimCallbacks ...ClaimCallback) *Decoder {
 }
 
 func (this Decoder) Decode(token string, claims interface{}) error {
+	// TODO if length of jwt after splitting != 3, bail with an error
+
 	data := strings.Split(token, ".")
+
+	header := data[0]
+	headerBytes, _ := base64.RawURLEncoding.DecodeString(header)
+	var headerValues map[string]interface{}
+	_ = json.Unmarshal(headerBytes, &headerValues)
+
 	payload := data[1]
 	payloadBytes, _ := base64.RawURLEncoding.DecodeString(payload)
+
+	if headerValues["alg"] != "none" {
+		signature := data[2]
+		signatureBytes, _ := base64.RawURLEncoding.DecodeString(signature)
+		hash := Hash(data[0]+"."+data[1], this.secret)
+		if subtle.ConstantTimeCompare(signatureBytes, hash) == 0 {
+			return errors.New("bad signature")
+		}
+	}
 
 	var parsedClaims map[string]interface{}
 	_ = json.Unmarshal(payloadBytes, &parsedClaims)
@@ -27,4 +47,8 @@ func (this Decoder) Decode(token string, claims interface{}) error {
 	}
 
 	return nil
+}
+
+func (this *Decoder) SetSecret(secret []byte) {
+	this.secret = secret
 }
--- decoder_test.go
+++ decoder_test.go
@@ -1,6 +1,8 @@
 package jwt
 
 import (
+	"encoding/base64"
+	"strings"
 	"testing"
 
 	"github.com/smartystreets/assertions/should"
@@ -20,7 +22,7 @@ func (this *DecoderFixture) Setup() {
 	this.decoder = NewDecoder(ParseIssuer, ParseExpiration)
 }
 
-func (this *DecoderFixture) TestDecode() {
+func (this *DecoderFixture) TestDecodeWithoutSignature() {
 	token := "eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ."
 
 	var claims parsedPayload
@@ -30,6 +32,52 @@ func (this *DecoderFixture) TestDecode() {
 	this.So(err, should.BeNil)
 	this.So(claims.Issuer, should.Equal, "joe")
 	this.So(claims.Expiration, should.Equal, 1300819380)
+}
+
+func (this *DecoderFixture) TestDecodeValidSignature() {
+	encoder := NewEncoder()
+	encoder.setAlgorithm("HS256")
+	secret := []byte("secret")
+	encoder.setSecret(secret)
+
+	token := encoder.Encode(rfcExample{
+		Issuer:     "joe",
+		Expiration: 1300819380,
+		IsRoot:     true,
+	})
+
+	var claims parsedPayload
+
+	this.decoder.SetSecret(secret)
+	err := this.decoder.Decode(token, &claims)
+
+	this.So(err, should.BeNil)
+	this.So(claims.Expiration, should.Equal, 1300819380)
+	this.So(claims.Issuer, should.Equal, "joe")
+}
+
+func (this *DecoderFixture) TestDecodeInvalidWellFormedSignature() {
+	encoder := NewEncoder()
+	encoder.setAlgorithm("HS256")
+	secret := []byte("secret")
+	encoder.setSecret(secret)
+	token := encoder.Encode(rfcExample{
+		Issuer:     "joe",
+		Expiration: 1300819380,
+		IsRoot:     true,
+	})
+
+	parsedToken := strings.Split(token, ".")
+	parsedToken[2] = base64.RawURLEncoding.EncodeToString(Hash("badToken", secret))
+	token = strings.Join(parsedToken, ".")
+
+	var claims parsedPayload
+
+	err := this.decoder.Decode(token, &claims)
+
+	this.So(err, should.NotBeNil)
+	this.So(claims.Expiration, should.Equal, 0)
+	this.So(claims.Issuer, should.BeBlank)
 }
 
 type parsedPayload struct {
