a39c42a3a1e0b4d766ac9b3f477bf1cedf9879c4
Michael Whatcott <mdwhatcott@gmail.com>
2019-10-10 15:04:32 -0600

    reformatting

--- contracts.go
+++ contracts.go
@@ -20,8 +20,10 @@ type Scope interface {
 	SetScope(string)
 }
 
-var SegmentCountErr = errors.New("a JWT must have three segments separated by period characters")
-var MalformedHeaderErr = errors.New("the header is malformed")
-var MalformedHeaderContentErr = errors.New("the header content is malformed")
-var MalformedPayloadContentErr = errors.New("the payload content is malformed")
-var MalformedSignatureErr = errors.New("the signature is malformed")
+var (
+	SegmentCountErr            = errors.New("a JWT must have three segments separated by period characters")
+	MalformedHeaderErr         = errors.New("the header is malformed")
+	MalformedHeaderContentErr  = errors.New("the header content is malformed")
+	MalformedPayloadContentErr = errors.New("the payload content is malformed")
+	MalformedSignatureErr      = errors.New("the signature is malformed")
+)
