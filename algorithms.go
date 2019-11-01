1f79c6cb4b8226a802279800cd8d89ddf49d16f1
Michael Whatcott <mdwhatcott@gmail.com>
2019-11-01 11:25:32 -0600

    Extracted function to eliminate duplication.

--- algorithms.go
+++ algorithms.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"crypto/sha512"
+	"hash"
 )
 
 type NoAlgorithm struct{}
@@ -13,29 +14,23 @@ func (this NoAlgorithm) ComputeHash(value, secret []byte) []byte { return nil }
 
 type HS256 struct{}
 
-func (this HS256) Name() string { return hs256 }
-func (this HS256) ComputeHash(value, secret []byte) []byte {
-	h := hmac.New(sha256.New, secret)
-	h.Write(value)
-	return h.Sum(nil)
-}
+func (this HS256) Name() string                            { return hs256 }
+func (this HS256) ComputeHash(value, secret []byte) []byte { return _hash(sha256.New, value, secret) }
 
 type HS384 struct{}
 
-func (this HS384) Name() string { return hs384 }
-func (this HS384) ComputeHash(value, secret []byte) []byte {
-	h := hmac.New(sha512.New384, secret)
-	h.Write(value)
-	return h.Sum(nil)
-}
+func (this HS384) Name() string                            { return hs384 }
+func (this HS384) ComputeHash(value, secret []byte) []byte { return _hash(sha512.New, value, secret) }
 
 type HS512 struct{}
 
-func (this HS512) Name() string { return hs512 }
-func (this HS512) ComputeHash(value, secret []byte) []byte {
-	h := hmac.New(sha512.New, secret)
-	h.Write(value)
-	return h.Sum(nil)
+func (this HS512) Name() string                            { return hs512 }
+func (this HS512) ComputeHash(value, secret []byte) []byte { return _hash(sha512.New, value, secret) }
+
+func _hash(algorithm func() hash.Hash, value, secret []byte) []byte {
+	hasher := hmac.New(algorithm, secret)
+	hasher.Write(value)
+	return hasher.Sum(nil)
 }
 
 const (
