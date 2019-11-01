fea3706e4aa8e8806b7fdc259076328e2c176197
Michael Whatcott <mdwhatcott@gmail.com>
2019-11-01 11:39:45 -0600

    Renamed function.

--- algorithms.go
+++ algorithms.go
@@ -15,19 +15,19 @@ func (this NoAlgorithm) ComputeHash(value, secret []byte) []byte { return nil }
 type HS256 struct{}
 
 func (this HS256) Name() string                            { return hs256 }
-func (this HS256) ComputeHash(value, secret []byte) []byte { return _hash(sha256.New, value, secret) }
+func (this HS256) ComputeHash(value, secret []byte) []byte { return _hmac(sha256.New, value, secret) }
 
 type HS384 struct{}
 
 func (this HS384) Name() string                            { return hs384 }
-func (this HS384) ComputeHash(value, secret []byte) []byte { return _hash(sha512.New, value, secret) }
+func (this HS384) ComputeHash(value, secret []byte) []byte { return _hmac(sha512.New, value, secret) }
 
 type HS512 struct{}
 
 func (this HS512) Name() string                            { return hs512 }
-func (this HS512) ComputeHash(value, secret []byte) []byte { return _hash(sha512.New, value, secret) }
+func (this HS512) ComputeHash(value, secret []byte) []byte { return _hmac(sha512.New, value, secret) }
 
-func _hash(algorithm func() hash.Hash, value, secret []byte) []byte {
+func _hmac(algorithm func() hash.Hash, value, secret []byte) []byte {
 	hasher := hmac.New(algorithm, secret)
 	hasher.Write(value)
 	return hasher.Sum(nil)
